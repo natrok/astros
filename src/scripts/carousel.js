477415da2f42801d1fa6e430a25a641e9ed75bfc
Diego Alexander Ramirez Buitrago <diegorok3@yahoo.es>
2025-04-02 20:52:33 +0200

    Finally pass the fuc.... parametres to an javascript file using alpine.js

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,21 +1,36 @@
-function carousel() {
-    return {
-      currentSlide: 0,
-        totalSlides: 4,
-        index: 0, 
-      items: ['Carta 88', 'Carta 2', 'Carta 3', 'Carta 4', 'Carta 5', 'Carta 6'],
-      init() {
-        //this.totalSlides = document.querySelectorAll('.carousel-card').length;
-        console.log(this.totalSlides);
-      },
+function test({texto}) {
+  // if (typeof currentItems !== 'object' || currentItems === null) {
+  if (typeof texto !== 'string' || texto === null) {
+    throw new TypeError('Expected an string');
+  }
+  return {
+    texto, // Esto sí devuelve un objeto válido
+    mensaje: `Mensaje: ${texto}`,
+  }
+}
 
-      next() { this.index = (this.index + 1) % this.totalSlides },      
-      prev() { this.index = (this.index - 1 + this.totalSlides) % this.totalSlides },
+function carousel(parametros) {
+  if (parametros === null) {
+     throw new TypeError('Expected an object');
+  }
+
+  return {     
+    currentSlide: 0,
+    totalSlides: 4,
+    index: 0, 
+    items: [],
+    init() {
+      this.items = parametros;
+    },
+   
+    next() { this.index = (this.index + 1) % this.totalSlides },      
+    prev() { this.index = (this.index - 1 + this.totalSlides) % this.totalSlides },
 
     startAutoplay() {
-            //setInterval(() => this.next(), 3000);
-          }
+       //setInterval(() => this.next(), 3000);
+      }
     };
 }
 // Hacerlo global para que Alpine pueda acceder
 window.carousel = carousel;
+window.test = test;
