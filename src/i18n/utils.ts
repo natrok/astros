28181d5978dc099a04d6ab948009299bb0cbfdda
Diego Alexander Ramirez Buitrago <diegorok3@yahoo.es>
2025-05-01 22:50:44 +0200

    Include FAQ page

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,6 +3,10 @@ import es from './es.json';
 import en from './en.json';
 import fr from './fr.json';
 
+import faq_es from './faq_es.json';
+import faq_fr from './faq_fr.json';
+import faq_en from './faq_en.json';
+
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
   if (lang in header) return lang as keyof typeof header;
@@ -21,3 +25,11 @@ export const geti18n = ({ currentLocale }: { currentLocale: string}) =>
   if (currentLocale === 'fr') return fr;
   return es; // Default to Spanish if no match found
 }
+
+export const getFAQi18n = ({ currentLocale }: { currentLocale: string}) =>
+  {
+    if (currentLocale === 'en') return faq_en;  
+    if (currentLocale === 'fr') return faq_fr;
+    return faq_es; // Default to Spanish if no match found
+  }
+  
